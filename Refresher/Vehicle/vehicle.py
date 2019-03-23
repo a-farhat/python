7ec5a0dae6120078f58559c3bfc68e76e467826f
a-farhat <akfarhat@gmail.com>
2019-03-23 03:45:51 +0200

    objectified vehicle

--- Refresher/Vehicle/vehicle.py
+++ Refresher/Vehicle/vehicle.py
@@ -44,23 +44,3 @@ class vehicle():
         self.vehicleMake = vehicleInfo[2]
         self.vehcileModel = vehicleInfo[3]
 
-
-
-
-
-def main():
-    #Instatiate vehicle here
-    #car = vehicle("Car",("",""),{"":"","":""})
-    highlander = ("Car","2014","Toyota","Highlander")
-    car = vehicle(highlander)
-    car.setDoorCount(4)
-    car.setNumberOFWheels(4)
-    car.setCapacity(7)
-    car.printVehicle()
-    car.start()
-    car.stop()
-
-
-
-if __name__ == "__main__":
-  main()
